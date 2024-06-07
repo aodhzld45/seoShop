9e12161b8c876bb44f4dc131b4d7ef2149660c12
서현석 <71122744+aodhzld45@users.noreply.github.com>
2024-06-07 15:38:33 +0900

    2024-06-07 카카오 로그인시 테스트용 회원가입

--- seofriends/src/main/java/com/seofriends/controller/KakaoController.java
+++ seofriends/src/main/java/com/seofriends/controller/KakaoController.java
@@ -1,5 +1,7 @@
 package com.seofriends.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seofriends.domain.MemberVO;
 import com.seofriends.kakao.KaKaoApi;
 import com.seofriends.kakao.KakaoApiService;
 import com.seofriends.kakao.KakaoUserInfoResponseDto;
@@ -44,16 +47,30 @@ public class KakaoController {
 		// 발급 받은 Access Token을 Bearer Token으로 인증.
 		KakaoUserInfoResponseDto userInfo = kakaoApiService.getUserInfo(accessToken); 
 		
+		MemberVO memberVO = new MemberVO();
+		memberVO.setMem_id(userInfo.getId().toString());
+		
+		memberVO.setMem_email(userInfo.getKakaoAccount().getEmail());
+		memberVO.setMem_nick(userInfo.getKakaoAccount().getProfile().getNickName());
+		
+		
+		System.out.println("memberVO에 Setting된 값 = " + memberVO);
+		
+		
+		// 카카오 회원가입 MemberService Join
+		if (memberVO.getMem_id() != null) {
+			memberService.join(memberVO);
+		}
+
+		
+		// 카카오 로그인 + 기존의 MemberService Login
+		
+		
+		
+		
+	
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 
