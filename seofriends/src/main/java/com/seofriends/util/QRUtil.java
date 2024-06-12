05cdfdedbea9135327000f8aaa4e45439d7edbcf
서현석 <71122744+aodhzld45@users.noreply.github.com>
2024-06-12 18:27:06 +0900

    2024-06-12 QRUtil QR코드를 읽는 메서드 추가

--- seofriends/src/main/java/com/seofriends/util/QRUtil.java
+++ seofriends/src/main/java/com/seofriends/util/QRUtil.java
@@ -2,6 +2,7 @@ package com.seofriends.util;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 import javax.imageio.ImageIO;
@@ -9,8 +10,15 @@ import javax.imageio.ImageIO;
 import org.springframework.stereotype.Component;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import lombok.extern.log4j.Log4j;
@@ -47,7 +55,22 @@ public class QRUtil {
 		return fileName;		
 	}
 	
-
 	
+	
+	
+	
+	// 생성된 QR 코드를 읽는 readQR Method
+	public static String readQR(String filePath) throws IOException, NotFoundException {
+		BufferedImage bufferedImage = ImageIO.read(new File(filePath));
+		LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+		
+		Result result = new MultiFormatReader().decode(bitmap);
+		
+		return result.getText();
+	
+	} 
+	
+
 	
 }
